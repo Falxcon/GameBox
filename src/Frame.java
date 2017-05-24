3a81c4debc74d00a2582577bf49423fd74124472
ValentinLutz <allround.email@gmail.com>
2017-05-24 09:02:23 +0200

    added Frame MenuBar

--- src/Frame.java
+++ src/Frame.java
@@ -5,8 +5,14 @@ import javax.swing.*;
 public class Frame extends JFrame{
 
 	private JDesktopPane desktopPane;
+
+	private JMenu menuAdd;
+
+	private JMenuItem menuItemGOL;
 	
 	Frame (){
+	    setJMenuBar(initMenuBar());
+
 		setTitle("Game Box");
 		setSize(1700, 900);
 		desktopPane = new JDesktopPane();
@@ -14,7 +20,20 @@ public class Frame extends JFrame{
 		setContentPane(desktopPane);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
-		desktopPane.add(new GOLMain(25, 25));
 	}
 
+    private JMenuBar initMenuBar() {
+	    JMenuBar menuBar = new JMenuBar();
+
+	    menuAdd = new JMenu("Add");
+        menuBar.add(menuAdd);
+
+	    menuItemGOL = new JMenuItem("GameOfLife");
+	    menuItemGOL.addActionListener(l -> {
+	        desktopPane.add(new GOLMain(25, 25));
+        });
+	    menuAdd.add(menuItemGOL);
+
+	    return menuBar;
+    }
 }
